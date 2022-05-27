a49f97e4053b1e4c9b9b48834437977a90d75953
daibeisi <heyares@163.com>
2022-05-28 00:22:29 +0800

    增加一个显示当前时间页面

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
-
+# 二级URL设置
 urlpatterns = [
     path('', views.index),
     path("test/", views.test),
+    path("time/", views.time),
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+import datetime
 
 
 # Create your views here.
@@ -9,3 +10,12 @@ def index(request):
 
 def test(request):
     return render(request, "base/test.html", {'hi': "你好", 'test': "测试"})
+
+
+def time(request):
+    now = datetime.datetime.now().now()
+    return HttpResponse("<div align='center'><h1>你好，欢迎你浏览本页面</h1><hr>当前时间是{year}年{month}月{day}日{hour}时"
+                        "{minute}分{second}秒{microsecond}<br></div>".format(year=now.year, month=now.month, day=now.day,
+                                                                           hour=now.hour, minute=now.minute,
+                                                                           second=now.second,
+                                                                           microsecond=now.microsecond))
