ce5844e7d0060fca18858ac818de0086b526d424
daibeisi <heyares@163.com>
2022-05-28 14:49:25 +0800

    修复页面访问favicon.ico报404

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
+from django.views.generic import RedirectView
+from django.templatetags.static import static
 from . import views
+
 
 # 二级URL设置
 urlpatterns = [
     path('', views.index),
     path("test/", views.test),
     path("time/", views.time),
+    path('favicon.ico', RedirectView.as_view(url=static('favicon.ico'))),
 ]
