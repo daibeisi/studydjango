61499028f625adaea7f727029bdd94d75ba14e77
daibeisi <heyares@163.com>
2022-05-28 22:21:02 +0800

    修改时间页面为使用模板渲染

--- base/views.py
+++ base/views.py
@@ -13,10 +13,13 @@ def test(request):
 
 def time(request):
     now = datetime.datetime.now().now()
-    return HttpResponse("<div align='center'><h1>你好，欢迎你浏览本页面</h1><hr>当前时间是{year}年{month}月{day}日{hour}时"
-                        "{minute}分{second}秒{microsecond}<br></div>".format(year=now.year, month=now.month, day=now.day,
-                                                                           hour=now.hour, minute=now.minute,
-                                                                           second=now.second,
-                                                                           microsecond=now.microsecond))
+    # return HttpResponse("<div align='center'><h1>你好，欢迎你浏览本页面</h1><hr>当前时间是{year}年{month}月{day}日{hour}时"
+    #                     "{minute}分{second}秒{microsecond}<br></div>".format(year=now.year, month=now.month,
+    #                                                                        day=now.day, hour=now.hour,
+    #                                                                        minute=now.minute, second=now.second,
+    #                                                                        microsecond=now.microsecond))
+    return render(request, "base/time.html", {'now': now})
+
+
 def test_filter(request):
     return render(request, "base/test_filter.html", {'hair': "morehair"})
