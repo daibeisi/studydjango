1495762a5137f5cea7c9fe452a307dc666515fc1
daibeisi <heyares@163.com>
2022-06-09 21:31:48 +0800

    添加Django forms测试

--- company/urls.py
+++ company/urls.py
@@ -5,24 +5,27 @@ from company.views import *
 app_name = "company"
 
 urlpatterns = [
+    path("", index, name="company_index"),
     # 部门管理
     path("list_dep/", list_dep, name="list_dep"),
-    re_path("del_dep/(?P<dep_id>[0-9])/", del_dep, name="del_dep"),
+    path("del_dep/<int:dep_id>/", del_dep, name="del_dep"),
     path("add_dep/", add_dep, name="add_dep"),
-    re_path("edit_dep/(?P<dep_id>[0-9])/", edit_dep, name="edit_dep"),
+    path("edit_dep/<int:dep_id>/", edit_dep, name="edit_dep"),
     # 员工信息管理
     path("list_employeeinfo/", list_employeeinfo, name="list_employeeinfo"),
-    re_path("del_employeeinfo/(?P<employeeinfo_id>[0-9])/", del_employeeinfo, name="del_employeeinfo"),
+    path("del_employeeinfo/<int:employeeinfo_id>/", del_employeeinfo, name="del_employeeinfo"),
     path("add_employeeinfo/", add_employeeinfo, name="add_employeeinfo"),
-    re_path("edit_employeeinfo/(?P<employeeinfo_id>[0-9])/", edit_employeeinfo, name="edit_employeeinfo"),
+    path("edit_employeeinfo/<int:employeeinfo_id>/", edit_employeeinfo, name="edit_employeeinfo"),
     # 员工管理
     path("list_employee/", list_employee, name="list_employee"),
-    re_path("del_employee/(?P<employee_id>[0-9])/", del_employee, name="del_employee"),
+    path("del_employee/<int:employee_id>/", del_employee, name="del_employee"),
     path("add_employee/", add_employee, name="add_employee"),
-    re_path("edit_employee/(?P<employee_id>[0-9])/", edit_employee, name="edit_employee"),
+    path("edit_employee/<int:employee_id>/", edit_employee, name="edit_employee"),
     # 小组管理
     path("list_group/", list_group, name="list_group"),
-    re_path("del_group/(?P<group_id>[0-9])/", del_group, name="del_group"),
+    path("del_group/<int:group_id>/", del_group, name="del_group"),
     path("add_group/", add_group, name="add_group"),
-    re_path("edit_group/(?P<group_id>[0-9])/", edit_group, name="edit_group"),
+    path("edit_group/<int:group_id>/", edit_group, name="edit_group"),
+    # 测试forms
+    path("test_forms/", test_forms, name="test_forms"),
 ]
