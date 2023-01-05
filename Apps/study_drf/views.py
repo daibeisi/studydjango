3c68b34cc4b49c4a12094eaa6a4cde8b7c9c2a53
daibeisi <daibeisi@outlook.com>
2023-01-05 22:08:42 +0800

    修改学习drf模块的serializers

--- Apps/study_drf/views.py
+++ Apps/study_drf/views.py
@@ -1,12 +1,11 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
-from .serializers import UserSerializer, GroupSerializer
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from .models import Snippet
-from .serializers import SnippetSerializer
+from .serializers import UserSerializer, GroupSerializer, SnippetSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
