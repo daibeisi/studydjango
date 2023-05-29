16d98b2a843aeb968de6a5bdc0e1372b96ecc230
daibeisi <heyares@163.com>
2023-05-29 10:03:04 +0800

    base添加信号量py

---
+++ Apps/base/signals.py
@@ -0,0 +1,14 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import UserInfo
+
+@receiver(post_save, sender=User)
+def create_user_userinfo(sender, instance, created, **kwargs):
+    if created:
+        UserInfo.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_userinfo(sender, instance, **kwargs):
+    instance.userinfo.save()
