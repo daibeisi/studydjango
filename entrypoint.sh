c987cc3e7732a1d74f5dc762abf7f120c38a0f7e
daibeisi <daibeisi@outlook.com>
2023-07-09 12:19:40 +0800

    修复

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ export PYTHONUNBUFFERED=1
 export VIRTUAL_ENV=/home/django/venv
 export PATH="/home/django/venv/bin:$PATH"
 
-python manage.py makemigrations --no-input && python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+#python manage.py makemigrations --no-input && python manage.py migrate --no-input
+#python manage.py collectstatic --no-input
 
 gunicorn -c gunicorn.conf.py
